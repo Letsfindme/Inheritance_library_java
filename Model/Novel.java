cdb92bdd37eb7d8073bc9bab9fe93dc285af2096
fadinoufal <noufal4me@gmail.com>
2018-07-30 16:04:02 +0200

    Operation of Goncour prize is added

--- Model/Novel.java
+++ Model/Novel.java
@@ -13,6 +13,20 @@ public class Novel extends Books {
     /**
      * 
      */
-    public String goncourtPrize;
+    public boolean goncourtPrize;
+    
+    public void goncourtPrize() {
+    	if (goncourtPrize == true) {
+    		System.out.println("This book has earned: Goncourt Prize");
+    	}
+    }
+
+	public boolean getGoncourtPrize() {
+		return goncourtPrize;
+	}
+
+	public void setGoncourtPrize(boolean goncourtPrize) {
+		this.goncourtPrize = goncourtPrize;
+	}
 
 }
