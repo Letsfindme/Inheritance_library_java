0c8674b10fbcfea43be571d6f25a1fba9bbaed3b
fadinoufal <noufal4me@gmail.com>
2018-07-30 16:03:19 +0200

    Operation of colored added

--- Model/Magazine.java
+++ Model/Magazine.java
@@ -14,4 +14,19 @@ public class Magazine extends Books {
      */
     public Boolean colored;
 
+    public void colored() {
+    	if (colored == true) {
+    		System.out.println("This book is in color");
+    	}else {
+    		System.out.println("This book is in black and white");
+    	}
+    }
+	public Boolean getColored() {
+		return colored;
+	}
+
+	public void setColored(Boolean colored) {
+		this.colored = colored;
+	}
+
 }
